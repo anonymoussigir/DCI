4a1b33348af950ba64168d0d73c076379192a387
anonymoussigir <zhang-jing@ruc.edu.cn>
2021-02-24 12:21:25 +0800

    update code

--- util.py
+++ util.py
@@ -4,7 +4,6 @@ import torch
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
-from collections import Counter
 
 random.seed(0)
 np.random.seed(0)
